f5c039bab67b3bf844c5daeb9b18875e94c025d3
Falk Schrewe <Falk.Schrewe@tu-dresden.de>
2022-08-17 08:15:22 +0200

    generelles update nach update der R Version

--- SCRIPT/IBF/Karte Plot Falk.R
+++ SCRIPT/IBF/Karte Plot Falk.R
@@ -8,8 +8,8 @@ z <- seq(1,100, 4)
 points(z,z, pch=22)
 
 ############################# GENERATE PLOTMAP #################################
-# J.Osewold
-# 12.04.22
+# F. R. Schrewe
+# 04.08.2022
 ##### DRAFT #####
 ################################################################################
 
@@ -26,8 +26,9 @@ Falls ja: ?
 ###### LIBRARYS ----------------------------------------------------------------
 
 ## 1. Have the computer install all required packages:                    ------
-
-packages=c("RODBC"  # Package RODBC implements ODBC database connectivity.
+packages=c("RODBC",   # Package RODBC implements ODBC database connectivity.
+					 "ggplot2", # A system for declaratively creating graphics.
+					 "dplyr"    # Helps you solve the most common data manipulation challenges.
 )
 
 
@@ -38,54 +39,34 @@ for (pkg in packages) {
 		install.packages(pkg)
 	}
 }
+
+
 dev.off(dev.list()["RStudioGD"])  # Delete graphics memory 
 rm(list = ls())                   # Delete workspace 
+gc(verbose = TRUE, reset = TRUE)  # Garbage collection
 cat("\014")		     	              # Clear console 
-gc(reset = TRUE)                  # Garbage collection
 
 
 ###### REQUIRES ----------------------------------------------------------------
 source(file = "SCRIPT/IBF/LOAD_DATA_TREE.R")
-	'-> Lädt die DFs der einzelnen IB+ Flächen'
+#  -> Lädt die DFs der einzelnen IB+ Flächen
+rm(list=setdiff(ls(), "data_tree_scho")) # Zur Übersichtlichkeit: Vorab alles löschen dass ich noch nicht brauche
 
 source(file = "SCRIPT/IBF/GENERATE_PLOTDISTRIBUTION.R")
-	'-> kreiert die x & y-Koordinaten der Subplots'
-
+#  -> kreiert die x & y-Koordinaten der Subplots
+rm(list=setdiff(ls(), c("plots_ref_scho", "plots_pos_scho", "data_tree_scho"))) # Zur Übersichtlichkeit: Vorab alles löschen dass ich noch nicht brauche
+	
 source(file = "SCRIPT/IBF/generate_plotmap.R")
-'	-> kreirt einen Plot-Befehl für die Karten, angepasst an Format der Ursprungsdaten'
+#  -> kreirt einen Plot-Befehl für die Karten, angepasst an Format der Ursprungsdaten
+
+source(file = "SCRIPT/IBF/LOAD_NV.R") 
+#  Loads and cleans all data from the Winter IBF surveys
 
 
 ###### NOTES -------------------------------------------------------------------
 
 
-###### SCHOTTEN  ---------------------------------------------------------------
-plot.all1 <- function(tree_data, plots_pos, plots_ref) {
-	
-	### MERGE ALL DATA -----------------------------------------------------------
-	all_points <- data.frame(
-		nr = c(data_tree_scho$nr, plots_pos_scho$nr, plots_ref_scho$nr),
-		x = c(data_tree_scho$x, plots_pos_scho$x, plots_ref_scho$x),
-		y = c(data_tree_scho$y, plots_pos_scho$y, plots_ref_scho$y),
-		type = c(rep("tree", nrow(data_tree_scho)), 
-						 rep("plot", nrow(plots_pos_scho)),
-						 rep("ref_target", nrow(plots_ref_scho)/2),
-						 rep("ref_current", nrow(plots_ref_scho)/2)))
-	
-	### SET COLOURS --------------------------------------------------------------
-	color <- all_points$type
-	color[color == "tree"] <- "gray"
-	color[color == "plot"] <- "black"
-	color[color == "ref_target"] <- "red"
-	color[color == "ref_current"] <- "green"
-	
-	### PLOT ---------------------------------------------------------------------
-	plot <- plot(all_points$x, all_points$y, col = color
-							 #, xlim = c(0,70), ylim = c(0,80)
-	)
-	return(plot)
-}
-
-df <- plot.all1("all_points")
+### SCHOTTEN  ---------------------------------------------------------------
 
 
 # Test ob das "auch so" geht
@@ -98,249 +79,59 @@ plot.all1(tree_data = data_tree_scho, plots_pos = plots_pos_scho,
          plots_ref = plots_ref_scho)
 plots_ref_scho
 
-c(data_tree_scho$nr, plots_pos_scho$nr, plots_ref_scho$nr)
 
 
 
 
+###### Create Dataframe  ---------------------------------------------------------------
 
-
+# Create Dataframe with Treedata from Schotten
 all_points <- data.frame(
 	nr = c(data_tree_scho$nr),
 	x = c(data_tree_scho$x),
 	y = c(data_tree_scho$y),
-	art = c(data_tree_scho$art),
+	art = c(data_tree_scho$art.stammv),
 	dia = c(data_tree_scho$dmess))
 
 
-
-
-
-
+# Clean Data
+# remove weird ones without Art
 all_points <- all_points[!is.na(all_points$art),]
-
-# remove weird one without Art
 all_points <- all_points[all_points$art!=0,]
 
 
-plot(all_points$x, all_points$y, col=1, pch=21, bg=adjustcolor(as.numeric(as.factor(all_points$art)), .5), cex = all_points$dia/100)
-plot(all_points$x, all_points$y, col=1, pch=21, bg=all_points$art+1, cex = all_points$dia/100)
-
-
-data_tree_scho$nr
-
+plot(all_points$x, all_points$y, col=1, pch=21, bg=adjustcolor(as.numeric(as.factor(all_points$art)), .5), cex = sqrt(all_points$dia/100))
 
 as.numeric(as.factor(all_points$art))
-
-plot(1,1, cex=10, pch=25,bg=adjustcolor("red", alpha.f=.5))
-
 table(all_points$art)
 
 
-## Hegyi ####
+
+
+
 
 all_points$xy <- all_points$x + 1i*all_points$y
-k<-2; plotarea<-10000
 
-
-CZR<-sqrt(10000/(nrow(all_points))/plotarea)*k  
-tree0<-all_points$nr
-nrow(all_points)
-
-x0<-NULL
-y0<-NULL
-spec0<-NULL
-d0<-NULL
-pos11<-NULL
-pos12<-NULL
-posxy<-NULL
-
-for(n in 1:length(tree0)){
-	x0[n]<-all_points$x[all_points$nr==tree0[n]]
-	y0[n]<-all_points$y[all_points$nr==tree0[n]]
-	spec0[n]<-all_points$Sp[all_points$nr==tree0[n]]
-	d0[n]<-all_points$dia[all_points$nr==tree0[n]]
-	pos11[n]<-all_points$xy[all_points$nr==tree0[n]]
-	pos12[n]<-all_points$xy[all_points$nr==tree0[n]]
-	posxy[n]<-all_points$xy[all_points$nr==tree0[n]]
-}
-
-x0
-y0
-spec0
-d0
-pos11
-pos12
-posxy
-
-ref_tree<-cbind(tree0, spec0, d0, x0, y0); ref_tree # all the ref trees 
-ref_tree<-as.data.frame(ref_tree)
-is(ref_tree)
-cbind(ref_tree, posxy)
-ref_tree <- cbind(ref_tree, posxy) # neu
-
-plot(all_points$xy, main="Treecoordinates Plot 1", #xlim=c(0,30),ylim = c(0,30),
+plot(all_points$xy, main="Stem distribution map \nSchotten", #xlim=c(0,30),ylim = c(0,30),
 		 xlab="Osten in Metern", ylab= "Norden in Metern",
 		 cex=sqrt(all_points$dia)/10)  
-
-for(n in 1:length(tree0)){
-	pos11<-ref_tree$posxy[n]
-	pos12<-all_points$xy[n]
-}
-is(ref_tree$posxy)
-is(tree0)
-dist<-(pos11-pos12)
-abs(dist)
-
-
-
-
-new1<-matrix(NA,0,2)
-
-for(n in 1:nrow(all_points)){
-	pos11[n]<-all_points$xy[n]
-	pos12[n]<-all_points$xy[n]
-	new1<-cbind(pos11, pos12)
-}
-
-
-x<-1*sin(2);x
-
-
-### all-all #####
-
-loc<-c(1:3,8:10,12,13)
-for(o in 1:length(loc)) {
-	
-	# PlotX <- all_points[all_points$LOC==(loc[o]), ]
-	PlotX <- all_points
-	
-	### ----------------------------------------------------------------------------
-	### Set Competition Zone Radii
-	
-	#k<-2 
-	#plotarea<-10000/(30*30) # fläche = 10000, plot = 30
-	#CZR<-sqrt(10000/(nrow(PlotX))/plotarea)*k
-	#print(CZR)
-	
-	#Alternativ Frank Hegyis Version mit 10 Fuß:
-	CZR<-3.05
-	
-	### ----------------------------------------------------------------------------
-	### Create empty Vectors to fill data.frame
-	
-	tree0<-PlotX$nr 
-	x0    <-PlotX$x 
-	y0    <-PlotX$y 
-	spec0 <-PlotX$art 
-	d0    <-PlotX$dia
-	ref_tree<-cbind.data.frame(tree0,x0, y0, spec0, d0)
-	
-	### ----------------------------------------------------------------------------
-	### Run loop for Hegyi Index
-	
-	hegyi <- rep(0, length(tree0))
-	test<-NULL
-	
-	for(n in 1:length(tree0)) {
-		bhd_i <- d0[n]
-		
-		for(i in 1:nrow(PlotX)) {
-			if(tree0[n] != PlotX$nr[i]) {
-				
-				xdiff <- x0[n] - PlotX$x[i]
-				ydiff <- y0[n] - PlotX$y[i]
-				
-				distance <- sqrt( xdiff^2 + ydiff^2 )
-				
-				if(distance <= CZR) {
-					bhd_target <- PlotX$dia[i]
-					
-					hegyi[n] = hegyi[n] + ( bhd_target / bhd_i ) / distance 
-				}
-			}
-		}
-	}
-	
-	
-	### ----------------------------------------------------------------------------
-	### Bind Hegyi with ref_tree and all_points
-	
-	ref_tree<-cbind(ref_tree, hegyi); ref_tree
-	
-	#all_points$Hegyi[all_points$LOC==loc[o]] <- ifelse(all_points$nr[all_points$LOC==loc[o]] %in% (ref_tree$tree0), ref_tree$hegyi, NA)#Produces wrong order
-	i1 <- with(all_points, match(nr[LOC==loc[o]], with(ref_tree, tree0)))
-	all_points$Hegyi[all_points$LOC==loc[o]] <- ref_tree$hegyi[i1]
-}
-
-
-
-#max(all_points[, 26], na.rm = TRUE)
-#write.csv2(all_points, file = "MyData.csv")
+# text(all_points$xy, labels = all_points$nr)
 
 
 
 
 
-df <- all_points
+df <- all_points; rm(all_points) # kein Bock mehr auf langen Namen
 sum(is.na(df$xy))
+sum(is.na(df$y))
+sum(is.na(df$x))
 
 nrow(df)-sum(is.na(df$xy))
 
 df <- df[complete.cases(df[,c("xy")]),] # Remove all Rows without XY Coordinates
 
 
-
-if(df[3,] != PlotX$nr[i]) {
-	
-	xdiff <- x0[n] - PlotX$x[i]
-	ydiff <- y0[n] - PlotX$y[i]
-	
-	distance <- sqrt( xdiff^2 + ydiff^2 )
-	
-	if(distance <= CZR) {
-		bhd_target <- PlotX$dia[i]
-		
-		hegyi[n] = hegyi[n] + ( bhd_target / bhd_i ) / distance 
-	}
-}
-
-
-zone <- 6 # Abstand die der Oberhöhe entspricht...
-focus <- 3 # Baum den ich zum ausprobieren betrachte
-
-for (i in 1:nrow(df)) {
-	print(
-		abs(df[focus,]$xy-df[i,]$xy)
-	)
-	arrows(Re(df[focus,]$xy),Im(df[focus,]$xy), Re(df[i,]$xy), Im(df[i,]$xy), length = 0, col = "purple")
-	ifelse(
-		abs(df[focus,]$xy-df[i,]$xy) <= zone,
-		arrows(Re(df[focus,]$xy),Im(df[focus,]$xy), Re(df[i,]$xy), Im(df[i,]$xy), length = 0, col = "red"),
-		NA
-	)
-}
-
-df[abs(df[x,]$xy-df$xy) <= 6,]
-
-df[3,]$xy
-
-
-arrows(Re(df[3,]$xy),Im(df[3,]$xy), Re(df[10,]$xy), Im(df[10,]$xy), length = 0, col = "purple")
-
-arrows(Re(df[focus,]$xy),Im(df[focus,]$xy), Re(df[i,]$xy), Im(df[i,]$xy), length = 0, col = "red")
-
-
-
-
-
-plot(all_points$xy, main="Treecoordinates Plot 1", #xlim=c(0,30),ylim = c(0,30),
-		 xlab="Osten in Metern", ylab= "Norden in Metern",
-		 cex=sqrt(all_points$dia)/10)  
-# text(all_points$xy, labels = all_points$nr)
-
-
-# allgemeiner Test
+# allgemeiner Test mit zufälligem Baum
 if (abs(df[3,]$xy-df[16,]$xy) <= 6) {
 	arrows(Re(df[3,]$xy),Im(df[3,]$xy), Re(df[16,]$xy), Im(df[16,]$xy), length = 0, col = "purple", lwd = 3)
 }
@@ -362,7 +153,7 @@ for (cc in 1:nrow(df)) {
 		}	
 	}
 	setTxtProgressBar(pb,cc)
-}; close(pb)
+}; close(pb); rm(pb, cc, xx)
 
 # Anstelle von Linien Zeichnen eine Vermerkung im DataFrame des Trupps einfügen
 df$gruppierung <- 0
@@ -374,42 +165,51 @@ for (cc in 1:nrow(df)) {
 		}	
 	}
 	setTxtProgressBar(pb,cc)
-}; close(pb)
+}; close(pb); rm(pb, cc, xx)
 table(df$gruppierung)
 
 
 # Okay. Soweit, so gut. Aber es geht ja um Truppen / Gruppen einer Art. Also mal Subset nach Art.
 
-table(df$art)
-d221 <- subset(df, art == "221")
-d211 <- subset(df, art == "211")
-d322 <- subset(df, art == "322")
-d321 <- subset(df, art == "321")
-d311 <- subset(df, art == "311")
-d110 <- subset(df, art == "110")
-d354 <- subset(df, art == "354")
-d323 <- subset(df, art == "323")
-d999 <- subset(df, art == "999")
-d811 <- subset(df, art == "811")
-d666 <- subset(df, art == "666")
-d555 <- subset(df, art == "555")
-d333 <- subset(df, art == "333")
-d342 <- subset(df, art == "342")
-d111 <- subset(df, art == "111")
+table(df$art)                    # Species                  # n
+d999 <- subset(df, art == "999") # Unbekannte Baumart        15
+d110 <- subset(df, art == "110") # Quercus spec.              2
+d111 <- subset(df, art == "111") # Quercus robur				   		2
+d211 <- subset(df, art == "211") # Fagus sylvatica           41
+d221 <- subset(df, art == "221") # Carpinus betulus         142
+d311 <- subset(df, art == "311") # Fraxinus excelsior        91
+d321 <- subset(df, art == "321") # Acer pseudoplatanus      132
+d322 <- subset(df, art == "322") # Acer platanoides          11
+d323 <- subset(df, art == "323") # Acer campestre             7
+d333 <- subset(df, art == "333") # Ulmus minor                1 
+d342 <- subset(df, art == "342") # Tilia  cordata             1
+d354 <- subset(df, art == "354") # Prunus avium               2
+d811 <- subset(df, art == "811") # Larix decidua              1
+d555 <- subset(df, art == "555") #                            5
+d666 <- subset(df, art == "666") # Hochsitz                   1
 
-plot(all_points$xy, main="Treecoordinates Plot 1", #xlim=c(0,30),ylim = c(0,30),
+# Was ist 555? vermutlich der Weg?
+plot(df$x, df$y, col=1, pch=21, bg=adjustcolor(as.numeric(as.factor(df$art)), .5), cex = sqrt(df$dia)/10)
+text(df[df$art=="555",]$x, df[df$art=="555",]$y); lines(df[df$art=="555",]$x, df[df$art=="555",]$y)
+
+aggregate(list(Anz = df$art), by = list(Art = df$art), FUN = length)
+
+# Löschen von Daten die ich nicht brauche
+rm("d555", "d666")
+
+plot(df$xy, main="Stem distribution map \nSchotten", #xlim=c(0,30),ylim = c(0,30),
 		 xlab="Osten in Metern", ylab= "Norden in Metern",
-		 cex=sqrt(all_points$dia)/10, pch = 1)
+		 cex=sqrt(df$dia)/10, pch = 1)
 
 # Versuch mit der Art 211
 
-# Test ob ich von allen Bäumen der Art 211 einzelnd auf die Umliegenden der Art 211 zugreifen kann
-pb = txtProgressBar(min = 0, max = nrow(d211), initial = 0, char = ":", style = 3) 
-for (cc in 1:nrow(d211)) {
+# Test ob ich von allen Bäumen der Art 311 einzelnd auf die Umliegenden der Art 211 zugreifen kann
+pb = txtProgressBar(min = 0, max = nrow(d311), initial = 0, char = ":", style = 3) 
+for (cc in 1:nrow(d311)) {
 	for (xx in 1:nrow(d211)) {
-		if (abs(d211[cc,]$xy-d211[xx,]$xy) <= 6) {  # Maximaler Abstand zwischen Bäumen der betrachteten Art in Metern
-			if (abs(d211[cc,]$xy-d211[xx,]$xy) > 0) { # Minimaler Abstand zwischen Bäumen der betrachteten Art in Metern
-			arrows(Re(d211[cc,]$xy),Im(d211[cc,]$xy), Re(d211[xx,]$xy), Im(d211[xx,]$xy), length = 0, col = "purple", lwd = 2)
+		if (abs(d311[cc,]$xy-d211[xx,]$xy) <= 6) {  # Maximaler Abstand zwischen Bäumen der betrachteten Art in Metern
+			if (abs(d311[cc,]$xy-d211[xx,]$xy) > 0) { # Minimaler Abstand zwischen Bäumen der betrachteten Art in Metern
+			arrows(Re(d311[cc,]$xy),Im(d311[cc,]$xy), Re(d211[xx,]$xy), Im(d211[xx,]$xy), length = 0, col = "purple", lwd = 2)
 			}
 		}
 	}
@@ -429,14 +229,13 @@ for (cc in 1:nrow(d211)) {
 
 
 
-# Alle Baumarten suchen nach Baumarten im Umkreis x
+# Alle Baumarten suchen nach Artgenosssen im Umkreis x
 
-plot(all_points$xy, main="Treecoordinates Plot 1", #xlim=c(0,30),ylim = c(0,30),
+plot(df$xy, main="Stem distribution map \nSchotten", #xlim=c(0,30),ylim = c(0,30),
 		 xlab="Osten in Metern", ylab= "Norden in Metern",
-		 cex=sqrt(all_points$dia)/10, pch = 1, col=as.numeric(as.factor(all_points$art)), lwd=2)
-points(all_points[all_points$art=="311",]$xy, cex=sqrt(all_points[all_points$art=="311",]$dia)/10, pch=21, bg=rgb(0,0,0,.5))
-
-ff <- list(d110, d111, d211, d221, d311, d321, d322, d323, d333, d342, d354, d555, d666, d811, d999)
+		 cex=sqrt(df$dia)/10, pch = 1, col=as.numeric(as.factor(df$art)), lwd=2, sub = "Eschen markiert")
+points(df[df$art=="311",]$xy, cex=sqrt(df[df$art=="311",]$dia)/10, pch=21, bg=rgb(0,0,0,.5))
+ff <- list(d110, d111, d211, d221, d311, d321, d322, d323, d333, d342, d354, d811, d999)
 # ff <- list(d221, d321)
 
 
@@ -479,8 +278,14 @@ table(d211$gruppierung)'
 26  6  7  2 
 26 haben keine Verbindung innerhalb der maximalen Distanz, 6 haben eine, 7 zwei und 2 haben drei Bäume um sich'
 
-points(d211[d211$gruppierung > 0,]$xy, pch = 16, cex = 6, col = adjustcolor(d211[d211$gruppierung > 0,]$gruppierung, .5))
-
+plot(df$xy, main="Stem distribution map \nSchotten", #xlim=c(0,30),ylim = c(0,30),
+		 xlab="Osten in Metern", ylab= "Norden in Metern",
+		 cex=sqrt(df$dia)/10, pch = 1)
+points(d211[d211$gruppierung > 0,]$xy, pch = 16, cex = sqrt(d211[d211$gruppierung > 0,]$dia)/10, col = adjustcolor(d211[d211$gruppierung > 0,]$gruppierung, .5))
+legend("bottomleft", inset = .02, legend=c(sort(unique(d211[d211$gruppierung > 0,]$gruppierung), decreasing = F)),
+			 bg = "white", 
+			 pch=21, pt.bg = c(adjustcolor(sort(unique(d211[d211$gruppierung > 0,]$gruppierung), decreasing = F), .5)), 
+			 pt.cex=1.5, y.intersp = .75, box.col = "aquamarine3", title = "Anzahl Nachbarn", text.font=11)
 
 
 ### Subplots #####
@@ -496,15 +301,15 @@ points(plots_pos_scho$xy, pch = 22, srt = 5)
 
 
 # Fokus Subplots: Wie viele Eschen sind im Umkreis?
-plot(all_points$xy, main="Treecoordinates Plot 1", #xlim=c(0,30),ylim = c(0,30),
+plot(df$xy, main="Stem distribution map \nSchotten", #xlim=c(0,30),ylim = c(0,30),
 		 xlab="Osten in Metern", ylab= "Norden in Metern",
-		 cex=sqrt(all_points$dia)/10, pch = 1, col=as.numeric(as.factor(all_points$art)), lwd=2)
+		 cex=sqrt(df$dia)/10, pch = 1, col=as.numeric(as.factor(df$art)), lwd=2)
 text(plots_pos_scho$xy, labels = "\u25FB", srt = 25)
 
 plots_pos_scho$umkreis <- 0
 for (cc in 1:nrow(plots_pos_scho)) {
-	for (xx in 1:nrow(d211)) {
-		if(abs(plots_pos_scho[cc,]$xy-d211[xx,]$xy)<= 6) {
+	for (xx in 1:nrow(d311)) {
+		if(abs(plots_pos_scho[cc,]$xy-d311[xx,]$xy)<= 6) {
 			plots_pos_scho[cc,]$umkreis <- plots_pos_scho[cc,]$umkreis + 1
 		}
 	}
@@ -530,7 +335,7 @@ for (cc in 1:nrow(plots_pos_scho)) {
 
 # Und nun für alle Baumarten nacheinander
 
-ff <- list(d110, d111, d211, d221, d311, d321, d322, d323, d333, d342, d354, d555, d666, d811, d999)
+ff <- list(d110, d111, d211, d221, d311, d321, d322, d323, d333, d342, d354, d811, d999)
 
 for (vv in 1:length(ff)) {
 	print("Durchlauf Nr.")
@@ -552,11 +357,11 @@ for (vv in 1:length(ff)) {
 # ------------------------------------------------------
 
 
-plot(all_points$xy, main="Treecoordinates Plot 1", #xlim=c(0,30),ylim = c(0,30),
+plot(df$xy, main="Stem distribution map \nSchotten", #xlim=c(0,30),ylim = c(0,30),
 		 xlab="Osten in Metern", ylab= "Norden in Metern",
-		 cex=sqrt(all_points$dia)/10, pch = 1, col=as.numeric(as.factor(all_points$art)), lwd=2)
+		 cex=sqrt(df$dia)/10, pch = 1, col=as.numeric(as.factor(df$art)), lwd=2)
 points(plots_pos_scho$xy, pch=22, bg=rgb(0,1,1,.5))
-
+text(plots_pos_scho$xy, labels = plots_pos_scho$nr)
 
 
 for (vv in 1:length(ff)) {            # Wechselt zwischen Baumarten
@@ -578,8 +383,84 @@ for (vv in 1:length(ff)) {            # Wechselt zwischen Baumarten
 	}
 }; close(pb); rm(pb)
 
-### Hier
 
+
+
+
+
+
+dfn <- data_nv[data_nv$Flaeche=="Schotten",]
+dd <- dfn[dfn$ID_plot=="79Sch" & dfn$Baumart_kurz=="GEs",]
+
+str(dd)
+
+table(dd$ETS)
+table(dfn$ID_plot)
+table(dfn$Plotnummer)
+
+sum(dd$ETS)
+length(dd$ETS)
+
+
+aggregate(list(Anz = dfn$Plotnummer), by = list(Plot = dfn$Plotnummer), FUN = length)
+
+
+# Anzahl Eschen gesamt
+plots_pos_scho$n.fe <- 0
+
+# Anzahl Eschen mit ETS
+plots_pos_scho$n.ets <- 0
+
+# Anzahl aller Bäume
+plots_pos_scho$n.ba <- 0
+
+head(plots_pos_scho)
+
+# Spalten mit Werten füllen
+plots_pos_scho[79,]$n.fe <- nrow(dfn[dfn$Plotnummer=="79" & dfn$Baumart_kurz=="GEs",])
+plots_pos_scho[79,]$n.ets <- nrow(dfn[dfn$Plotnummer=="79" & dfn$ETS==TRUE,])
+plots_pos_scho[79,]$n.ba <- nrow(dfn[dfn$Plotnummer=="79",])
+
+
+
+# Als schleife:
+for (cc in 1:nrow(plots_pos_scho)){
+	plots_pos_scho[cc,]$n.fe <- nrow(dfn[dfn$Plotnummer==cc & dfn$Baumart_kurz=="GEs",])
+	plots_pos_scho[cc,]$n.ets <- nrow(dfn[dfn$Plotnummer==cc & dfn$ETS==TRUE,])
+	plots_pos_scho[cc,]$n.ba <- nrow(dfn[dfn$Plotnummer==cc,])
+}
+
+
+# Da sind aber noch die ohne Bäume drin.
+
+
+for (cc in 1:nrow(plots_pos_scho)){
+	if((dfn[dfn$Plotnummer==cc,]$keine.baeume==TRUE)[1]){ # [1] Weil wenn ein Subplot keine Bäume hat, also FALSE ist und mehrere Bäume hat nimmt er nur den ersten.. sind ja eh alle FALSE
+		print(cc)
+		plots_pos_scho[cc, names(plots_pos_scho) %in% c("n.fe", "n.ba")] <- plots_pos_scho[cc, names(plots_pos_scho) %in% c("n.fe", "n.ba")] -1
+	} else{}
+}
+
+
+plots_pos_scho$prozent <- (plots_pos_scho$n.ets / plots_pos_scho$n.fe)*100
+plots_pos_scho
+## Den bonitierschlüssel anwenden... sonst wird das nichts
+
+
+unique(dfn$Baumart_lang)
+
+sum(is.na(dfn$Baumart_lang))
+
+dfn$Anzahl.Triebe
+dfn$ETS.lebend[dfn$Baumart_kurz=="GEs"]
+dfn$ETS.abgestorben.frisch+
+dfn[dfn$Baumart_kurz=="GEs",]$ETS.abgestorben.alt
+dfn[dfn$Baumart_kurz=="GEs",]$Baumart_lang
+
+dddd <- dfn[dfn$Baumart_kurz=="GEs",]
+dddd[12,]
+### Hier #####
+view(df)
 
 
 ###### MOLLENFELDE  ------------------------------------------------------------
