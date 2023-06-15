f2265bb3adb5902a423e3a67b998f86bbf9f8d0c
Johannes Osewold <josewold@nfv.nw-fva.de>
2023-06-15 16:29:32 +0200

    Minor changes

--- SCRIPT/IBF/Distance_ash_ets_falk.R
+++ SCRIPT/IBF/Distance_ash_ets_falk.R
@@ -1,8 +1,8 @@
 
-data_nv_plots <- read.csv(file = "EXPORT/IBF/tables/data_nv_plots_with_coord.csv",
-													fileEncoding = "UTF-8")
-data_tree_ibf <- read.csv(file = "DATA/RAW/ibf_alltrees_final_10-2022.csv")
-
+# data_nv_plots <- read.csv(file = "EXPORT/IBF/tables/data_nv_plots_with_coord.csv",
+# 													fileEncoding = "UTF-8")
+# data_tree_ibf <- read.csv(file = "DATA/RAW/ibf_alltrees_final_10-2022.csv")
+# 
 tmp <- data_nv_plots %>% 
 	mutate(ets_ratio = n_ets_total / n_ash)
 
@@ -33,17 +33,40 @@ for (i in 1:nrow(tmp)) {
 	ash_trees$dist <- sqrt(ash_trees$dist_x^2 + ash_trees$dist_y^2)
 	ash_trees <- ash_trees %>% 
 		arrange(dist)
+	nearest <- ash_trees [1:10,]
+	nearest <- nearest %>% 
+		mutate(value1  = 1/(dist / BHD_CM)) %>% 
+		mutate(value2  = dist) %>% 
+		mutate(value3 = 1/(dist / BHD_CM))
+	value1 <- sum(nearest$value1)
+	value2 <- sum(nearest$value2[1:5])
+	value3 <- sum(nearest$value3[1:3])
+	tmp$value1[i] <- value1
+	tmp$value2[i] <- value2
+	tmp$value3[i] <- value3
+}
+
+for (i in 1:nrow(tmp)) {
+	area <- tmp$Flaeche[i]
+	ash_trees <- tmp_tree %>% 
+		filter(flaeche == area) 
+		#filter(id_baumart == 120)
+	ash_trees$dist_x <- ash_trees$x_utm32 - tmp$x[i]
+	ash_trees$dist_y <- ash_trees$y_utm32 - tmp$y[i]
+	ash_trees$dist <- sqrt(ash_trees$dist_x^2 + ash_trees$dist_y^2)
+	ash_trees <- ash_trees %>% 
+		arrange(dist)
 	nearest <- ash_trees [1:5,]
 	nearest <- nearest %>% 
 		mutate(value1  = 1/(dist / BHD_CM)) %>% 
 		mutate(value2  = dist) %>% 
 		mutate(value3 = 1/(dist / BHD_CM))
 	value1 <- sum(nearest$value1)
-	value2 <- sum(nearest$value2[1:2])
-	value3 <- sum(nearest$value3[1:2])
-	tmp$value[i] <- value1
-	tmp$value2[i] <- value2
-	tmp$value3[i] <- value3
+	value2 <- sum(nearest$value2[1:5])
+	value3 <- sum(nearest$value3[1:3])
+	tmp$light_value[i] <- value1
+	tmp$light_value2[i] <- value2
+	tmp$light_value3[i] <- value3
 }
 
 
@@ -51,7 +74,7 @@ tmp2 <- tmp %>%
 	filter(n_ash != 0) %>% 
 	filter(ets_ratio <= .5) %>% 
 	filter(value >= 0.01) %>%
-	filter(value <= 50) %>% 
+	filter(value <= 100) %>% 
 	filter(ets_ratio != 0) 
 str(tmp2)
 ggplot(data = tmp2) +
@@ -61,7 +84,7 @@ tmp2 <- tmp %>%
 	filter(n_ash != 0) %>% 
 	filter(ets_ratio <= .5) %>% 
 	# filter(value >= 0.01) %>%
-	filter(value2 <= 50) %>% 
+	filter(value2 <= 500) %>% 
 	filter(ets_ratio != 0) 
 str(tmp2)
 ggplot(data = tmp2) +
@@ -77,4 +100,23 @@ str(tmp2)
 ggplot(data = tmp2, aes(x = value3, y = ets_ratio, size = n_ash, alpha = n_ash)) +
 	geom_point() 
 	#smooth()
+
+tmp2 <- tmp %>% 
+	filter(light_value >= 0.1, light_value <= 100)
+ggplot(data = tmp2, aes(x = sqrt(light_value), y = n_trees)) +
+	geom_point() +
+	geom_smooth()
+
+
+tmp2 <- tmp %>% 
+#	filter(n_ash != 0) %>% 
+#	filter(ets_ratio <= .5) %>% 
+	filter(value1 >= 0.01) %>%
+	filter(value1 <= 75) #%>% 
+#	filter(ets_ratio != 0) 
+str(tmp2)
+ggplot(data = tmp2, aes(x = value1, y = n_ash)) +
+	geom_point() +
+	geom_smooth()
+#smooth()
 
